93d2958bc62a0948386591b4eb602fbce3f89da6
caiorafaeldop <caiorafaeldop@gmail.com>
2023-08-29 11:48:14 -0300

    caioCommit2

--- src/modules/controler.py
+++ src/modules/controler.py
@@ -1,13 +1,14 @@
 from modules.buyer.buyer import addBuyer
 from modules.supermarket.supermarket import addSupermarket
 
+
 def ControllerBuyer(instancias=[]):
     emails = []
     loop = True
     while (loop == True):
         print("Selecione uma opção:\n1 - Adicionar Comprador\n2 - Sair")
         choose = input("")
-        
+
         if (choose == '1'):
             buyerData = addBuyer()
             if (buyerData.email not in emails):
@@ -23,26 +24,24 @@ def ControllerBuyer(instancias=[]):
 
 
 def ControllerSupermarket(instancias=[]):
-    cnpjs = []
+    #cnpjs = []
     logins = []
     loop = True
     while (loop == True):
         print("Selecione uma opção:\n1 - Adicionar Supermercado\n2 - Sair")
         choose = input("")
-        
+
         if choose == '1':
             supermarketData = addSupermarket()
 
-            
             # Verifica se o email e o CNPJ já estão em uso
-            if supermarketData.login not in logins and supermarketData.cnpj not in cnpjs:
+            if (supermarketData.login not in logins):
                 instancias.append(supermarketData)
                 logins.append(supermarketData.login)
-                cnpjs.append(supermarketData.cnpj)
                 print("Supermercado adicionado com sucesso\n \n")
             else:
                 print(
-                    '\nNão foi possível adicionar o supermercado. Login ou CNPJ já em uso\n')
+                    '\nNão foi possível adicionar o supermercado. Login já em uso\n')
 
         else:
             loop = False
@@ -50,14 +49,14 @@ def ControllerSupermarket(instancias=[]):
     return
 
 
-def mainControll(instancias = {"buyer": [], "supermarkets": []}):
+def mainControll(instancias={"buyer": [], "supermarkets": []}):
     print("Bem-vindo\nPor favor selecione uma opção para acessar nosse sistema.")
-    
+
     loop = True
-    while(loop == True):
+    while (loop == True):
         print("1 - Listar todos usuários do sistema\n2 - Acessar Area de Clientes \n3 - Acessar Area de Fornecedores\n4 - Encerrar")
         answer = input("Digite uma opção: ")
-        if(answer == "1"):
+        if (answer == "1"):
             print("Clientes")
             for i in instancias['buyer']:
                 i.print_informations()
@@ -65,14 +64,13 @@ def mainControll(instancias = {"buyer": [], "supermarkets": []}):
             for e in instancias['supermarkets']:
                 e.print_informations()
             print('\n')
-        elif(answer == '2'):
+        elif (answer == '2'):
             ControllerBuyer(instancias['buyer'])
-                
 
-        elif(answer == '3'):
+        elif (answer == '3'):
             ControllerSupermarket(instancias["supermarkets"])
-            
+
         else:
             loop = False
             break
-    return        
+    return
