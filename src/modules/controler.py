500fa1a39b1f4c8646db4fc9c10cd503c6a443f6
analaurarufino <analaurarufinosouza3@gmail.com>
2023-08-29 11:39:56 -0300

    adjusts

--- src/modules/controler.py
+++ src/modules/controler.py
@@ -1,16 +1,15 @@
 from modules.buyer.buyer import addBuyer
 from modules.supermarket.supermarket import addSupermarket
 
-
 def ControllerBuyer(instancias=[]):
     emails = []
     loop = True
-    while (loop):
-        print("Selecione uma opção:\n1 - Adicionar Comprador\n2 - Sair\n")
+    while (loop == True):
+        print("Selecione uma opção:\n1 - Adicionar Comprador\n2 - Sair")
         choose = input("")
+        
         if (choose == '1'):
             buyerData = addBuyer()
-            key = "key_" + buyerData.name
             if (buyerData.email not in emails):
                 instancias.append(buyerData)
                 emails.append(buyerData.email)
@@ -19,28 +18,26 @@ def ControllerBuyer(instancias=[]):
                 print('\nNão foi possível adicionar o comprador. Email já em uso\n')
         else:
             loop = False
-
-    for elem in instancias:
-        elem.print_informations()
+            break
+    return
 
 
 def ControllerSupermarket(instancias=[]):
     cnpjs = []
-    emails = []
+    logins = []
     loop = True
-    while loop:
-        print("Selecione uma opção:\n1 - Adicionar Supermercado\n2 - Sair\n")
+    while (loop == True):
+        print("Selecione uma opção:\n1 - Adicionar Supermercado\n2 - Sair")
         choose = input("")
-
+        
         if choose == '1':
             supermarketData = addSupermarket()
 
-            key = "key_" + supermarketData.name
-
+            
             # Verifica se o email e o CNPJ já estão em uso
-            if supermarketData.login not in emails and supermarketData.cnpj not in cnpjs:
+            if supermarketData.login not in logins and supermarketData.cnpj not in cnpjs:
                 instancias.append(supermarketData)
-                emails.append(supermarketData.login)
+                logins.append(supermarketData.login)
                 cnpjs.append(supermarketData.cnpj)
                 print("Supermercado adicionado com sucesso\n \n")
             else:
@@ -49,6 +46,33 @@ def ControllerSupermarket(instancias=[]):
 
         else:
             loop = False
+            break
+    return
 
-    for elem in instancias:
-        elem.print_informations()
+
+def mainControll(instancias = {"buyer": [], "supermarkets": []}):
+    print("Bem-vindo\nPor favor selecione uma opção para acessar nosse sistema.")
+    
+    loop = True
+    while(loop == True):
+        print("1 - Listar todos usuários do sistema\n2 - Acessar Area de Clientes \n3 - Acessar Area de Fornecedores\n4 - Encerrar")
+        answer = input("Digite uma opção: ")
+        if(answer == "1"):
+            print("Clientes")
+            for i in instancias['buyer']:
+                i.print_informations()
+            print("\nFornecedores")
+            for e in instancias['supermarkets']:
+                e.print_informations()
+            print('\n')
+        elif(answer == '2'):
+            ControllerBuyer(instancias['buyer'])
+                
+
+        elif(answer == '3'):
+            ControllerSupermarket(instancias["supermarkets"])
+            
+        else:
+            loop = False
+            break
+    return        
