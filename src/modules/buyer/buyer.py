8dc1d12e9aff7115ae18d75ed9bd0f581b4679aa
caiorafaeldop <caiorafaeldop@gmail.com>
2023-08-28 21:33:49 -0300

    caioCommit

--- src/modules/buyer/buyer.py
+++ src/modules/buyer/buyer.py
@@ -1,5 +1,5 @@
 class Buyer:
-    def __init__(self, name, email, password, favoriteSupermarkets = [], shoppingList = []):
+    def __init__(self, name, email, password, favoriteSupermarkets=[], shoppingList=[]):
         self.name = name
         self.email = email
         self.password = password
@@ -8,19 +8,19 @@ class Buyer:
 
     def validate_data(self):
         if not self.name:
-            return 1 
-        
+            return 1
+
         if "@" not in self.email or "." not in self.email:
-            return 2 
-        
+            return 2
+
         if len(self.password) < 8 or not any(c.isupper() for c in self.password):
-            return 3 
-        
+            return 3
+
         return 0
 
     def print_informations(self):
-        print("{\n" "  name: ",self.name,"\n  email: ",self.email,"\n  password: ",self.password, "\n" "}\n")
-
+        print("{\n" "  name: ", self.name, "\n  email: ", self.email,
+              "\n  password: ", self.password, "\n" "}\n")
 
 
 def addBuyer():
@@ -29,15 +29,15 @@ def addBuyer():
     password = input("Senha: ")
 
     buyer = Buyer(name, email, password, [], [])
-    
-    while(buyer.validate_data() != 0):
-        if(buyer.validate_data() == 1):
+
+    while (buyer.validate_data() != 0):
+        if (buyer.validate_data() == 1):
             print("Nome não pode ser vazio")
             buyer.name = input("Nome: ")
-        elif(buyer.validate_data() == 2):
+        elif (buyer.validate_data() == 2):
             print("Email inválido")
             buyer.email = input("Email: ")
-        elif(buyer.validate_data() == 3):
+        elif (buyer.validate_data() == 3):
             print("Senha deve ter 8 caracteres e conter pelo menos uma letra maiúscula")
             buyer.password = input("Senha: ")
             print(buyer.password)
--- src/modules/controler.py
+++ src/modules/controler.py
@@ -1,20 +1,52 @@
 from modules.buyer.buyer import addBuyer
+from modules.supermarket.supermarket import addSupermarket
 
-def ControllerBuyer(instancias = []):
+
+def ControllerBuyer(instancias=[]):
     emails = []
     loop = True
-    while(loop):
+    while (loop):
         print("Selecione uma opção:\n1 - Adicionar Comprador\n2 - Sair\n")
         choose = input("")
-        if(choose == '1'):
+        if (choose == '1'):
             buyerData = addBuyer()
             key = "key_" + buyerData.name
-            if(buyerData.email not in emails):
+            if (buyerData.email not in emails):
                 instancias.append(buyerData)
                 emails.append(buyerData.email)
                 print("Comprador adicionado com sucesso\n \n")
             else:
                 print('\nNão foi possível adicionar o comprador. Email já em uso\n')
+        else:
+            loop = False
+
+    for elem in instancias:
+        elem.print_informations()
+
+
+def ControllerSupermarket(instancias=[]):
+    cnpjs = []
+    emails = []
+    loop = True
+    while loop:
+        print("Selecione uma opção:\n1 - Adicionar Supermercado\n2 - Sair\n")
+        choose = input("")
+
+        if choose == '1':
+            supermarketData = addSupermarket()
+
+            key = "key_" + supermarketData.name
+
+            # Verifica se o email e o CNPJ já estão em uso
+            if supermarketData.login not in emails and supermarketData.cnpj not in cnpjs:
+                instancias.append(supermarketData)
+                emails.append(supermarketData.login)
+                cnpjs.append(supermarketData.cnpj)
+                print("Supermercado adicionado com sucesso\n \n")
+            else:
+                print(
+                    '\nNão foi possível adicionar o supermercado. Login ou CNPJ já em uso\n')
+
         else:
             loop = False
 
