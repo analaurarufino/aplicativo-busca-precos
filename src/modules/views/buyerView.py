3c9a6be86222068252f7a75c6630bf236cb22a71
Analaura Rufino <analaurarufinosouza3@gmail.com>
2023-10-05 21:18:23 -0300

    feature/views

---
+++ src/modules/views/buyerView.py
@@ -0,0 +1,10 @@
+from index import BaseView
+
+
+class BuyerView(BaseView):
+    def get_information(self):
+        self.display_output("Digite as informações do Comprador: ")
+        name = self.get_input("Nome: ")
+        email = self.get_input("Email: ")
+        password = self.get_input("Senha: ")
+        return {"name": name, "email": email, "password": password}
