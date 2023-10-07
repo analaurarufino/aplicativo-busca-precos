2fe3eff0a5c6033a6ce8f91127be599c5460f9a9
Carolinyarruda <carolinyarruda10@gmail.com>
2023-10-07 17:15:57 -0300

    firstcarol

--- src/modules/database/database.py
+++ src/modules/database/database.py
@@ -1,13 +1,33 @@
 import mysql.connector
 
-def connectDB():
-    keys = open('database.txt')
-    host = keys.readline().split('=')[1]
-    username = keys.readline().split('=')[1]
-    password = keys.readline().split('=')[1]
-    mydb = mysql.connector.connect(
-    host=host,
-    user=username,
-    password=password
-    )
-    return mydb
+from abc import ABC, abstractmethod
+
+class Database(ABC):
+    
+    @abstractmethod
+    def connectDB(self):
+        pass
+
+class Connect(Database):
+    def connectDB(self):
+        keys = open('database.txt')
+        host = keys.readline().split('=')[1].strip()
+        username = keys.readline().split('=')[1].strip()
+        password = keys.readline().split('=')[1].strip()
+        mydb = mysql.connector.connect(
+            host=host,
+            user=username,
+            password=password
+        )
+        return mydb
+
+
+class DataPersistence:         
+    def create_table(self):
+    def insert(self, mydb):
+    def update(self,mydb):
+    def delete(self,):
+    def list_all(self):
+    def research(self,):
+    def disconnectDB(self):
+        
