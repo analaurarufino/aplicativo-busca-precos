73cc52dbc27a81f90905bc6dbb47b66be5d29e20
analaurarufino <analaurarufinosouza3@gmail.com>
2023-11-10 13:17:00 -0300

    projeto final

--- src/modules/validation/error.py
+++ src/modules/validation/error.py
@@ -1,3 +1,5 @@
+#Padrão builder
+
 class CustomError(Exception):
     def __init__(self, message="Um erro ocorreu."):
         self.message = message
@@ -5,3 +7,19 @@ class CustomError(Exception):
 
     def __str__(self):
         return f"Error: {self.message}"
+
+class ErrorBuilder:
+    def __init__(self, base_message="Um erro ocorreu."):
+        self.custom_error = CustomError(base_message)
+
+    def add_details(self, details):
+        self.custom_error.message += f" Detalhes: {details}"
+        return self
+
+    def add_code(self, error_code):
+        self.custom_error.message += f" Código do erro: {error_code}"
+        return self
+
+    def build(self):
+        return self.custom_error
+
